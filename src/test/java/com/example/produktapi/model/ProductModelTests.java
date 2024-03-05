d7a8474e196531bf4749c79747553ecbf001e401
korsb <korsbacken@gmail.com>
2024-03-05 17:27:36 +0100

    Adding test methods for Product model

--- src/test/java/com/example/produktapi/model/ProductModelTests.java
+++ src/test/java/com/example/produktapi/model/ProductModelTests.java
@@ -6,10 +6,52 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ProductModelTests {
 
+    private Product product;
+
+    @BeforeEach
+    void setup(){
+        product = new Product("Test Title", 99.99, "Test Category", "This is a test description","image.jpg");
+    }
+
+    @Test
+    @DisplayName("Testing Title get-method")
+    void getTitleTest(){
+        Assertions.assertEquals("Test Title", product.getTitle());
+    }
+
+    @Test
+    @DisplayName("Testing Title set-method with correct value")
+    void setTitleCorrectValue(){
+        product.setTitle("New value");
+        Assertions.assertEquals("New value", product.getTitle());
+    }
+
+    @Test
+    @DisplayName("Testing Title set-method with special characters")
+    void setTitleWithSpecialCharacters(){
+        product.setTitle("!#¤%=?@£$€");
+        Assertions.assertEquals("!#¤%=?@£$€", product.getTitle());
+    }
+
+    @Test
+    @DisplayName("Testing Title set-method with empty string")
+    void setTitleWithEmptyString(){
+        product.setTitle("");
+        Assertions.assertEquals("", product.getTitle());
+    }
+
+    @Test
+    @DisplayName("Testing Title set-method with null")
+    void setTitleWithNull(){
+        product.setTitle(null);
+        Assertions.assertNull(product.getTitle());
+    }
+
     // Adding a test method just for testing imports and packages
     @Test
     @DisplayName("Testing")
     void testMethod(){
         Assertions.assertTrue(true);
     }
+
 }
